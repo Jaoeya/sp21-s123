d4338f13b57ab9c563e711ab3983b6941516a455
Jiaxing Zhou <jzhou92@sheffield.ac.uk>
2021-12-30 19:10:03 +0800

    done with lab1

--- lab1/GetEnvironmentVariables.java
+++ lab1/GetEnvironmentVariables.java
@@ -3,10 +3,10 @@ import java.util.Properties;
 public class GetEnvironmentVariables {
 
     //  Fill in the path to your sp21-s*** folder between the quotes
-    public static String REPO_DIR = "/Users/zhou/Desktop/skeleton-sp21";
+    public static String REPO_DIR = "/Users/zhou/Desktop/sp21-s123";
 
     //  Fill in the path to your snaps-sp21-s*** folder between the quotes
-    public static String SNAPS_DIR = "/Users/zhou/Desktop/snaps-sp21-zhou";
+    public static String SNAPS_DIR = "/Users/zhou/Desktop/snaps-sp21-s123";
 
     // Fill in the type of your shell by running 'echo $0` in your terminal. It should be zsh or bash.
     public static String SHELL = "bash";
